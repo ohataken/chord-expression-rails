4b5a40fbae420923c5f019a48eb687d232a185f1
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-06 19:37:36 +0900

    feat: root_notes_chords#index

--- app/controllers/root_notes_chords_controller.rb
+++ app/controllers/root_notes_chords_controller.rb
@@ -1,7 +1,16 @@
 class RootNotesChordsController < ApplicationController
   def index
+    @chords = modifier_names.map do |modifier_name|
+      ChordExpression::ChordParser.new(params[:root_note_name] + modifier_name).parse
+    end
   end
 
   def show
+  end
+
+  private
+
+  def modifier_names
+    %W[mmaj7 min7 maj7 dim7 7 min maj aug dim]
   end
 end
