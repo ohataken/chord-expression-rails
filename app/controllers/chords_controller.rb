c76dbf9ec327c563b4ab11ab14cc71e5c78a84fe
ohataken <11092425+ohataken@users.noreply.github.com>
2025-07-06 12:28:27 +0900

    feat: chords#show

--- app/controllers/chords_controller.rb
+++ app/controllers/chords_controller.rb
@@ -1,4 +1,11 @@
 class ChordsController < ApplicationController
   def show
+    @chord = chord_parser.parse
+  end
+
+  private 
+
+  def chord_parser
+    ChordExpression::ChordParser.new(params[:name])
   end
 end
